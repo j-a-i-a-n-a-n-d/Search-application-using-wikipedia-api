1fd1ea90b3a438375766af6f590b1bce4acca0bc
jai <jaianand5789@gmail.com>
2022-07-16 21:02:28 +0530

    initial commit

---
+++ theme toggler/js/script.js
@@ -0,0 +1,26 @@
+let themeToggler = document.querySelector('.theme-toggler');
+
+themeToggler.onclick = () =>{
+
+    themeToggler.classList.toggle('active');
+
+    if(themeToggler.classList.contains('active')){
+        document.body.classList.add('active');
+        document.getElementById("search").style.backgroundColor = "#333333";
+    }else{
+        document.body.classList.remove('active');
+        document.getElementById("search").style.backgroundColor = "#fff";
+    }
+
+}
+let theme = document.querySelector('.themes-container');
+
+document.querySelector('#theme-open').onclick = () =>{
+    theme.classList.add('active');
+    document.body.style.paddingRight = '350px';
+}
+
+document.querySelector('#theme-close').onclick = () =>{
+    theme.classList.remove('active');
+    document.body.style.paddingRight = '0px';
+}
